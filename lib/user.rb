51982d7fa6ab550e4efd3d9d04c7d3f0dfcf1780
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-03 03:03:07 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,10 @@
 class User 
+attr_accessor :first_name, :last_name, :knowledge
+ 
+ def initializer(first_name, last_name)
+   @first_name = first_name
+   @last_name = last_name
+   @knowledge = []
+ end 
   
 end
