ae728fc4c7f7119982831ad53111dfd16e466e87
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-03 03:01:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,7 @@
 class Student < User
- attr_accessor :first_name, :last_name
+ attr_accessor :first_name, :last_name, :knowledge
+ 
+ @knowledge = []
  
  def initializer(first_name, last_name)
    @first_name = first_name
